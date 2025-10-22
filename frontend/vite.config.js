7b1566a8ebfa5e59e60998306eb044ff7900c55d
sarveshkumbharde <165779142+sarveshkumbharde@users.noreply.github.com>
2025-10-22 12:53:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      // This helps with local development
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
 })
